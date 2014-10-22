a12f7f0b2a4aa01e6bd5206e004d289453fe43ae
Flavio Pereira <flaviopereira@g.harvard.edu>
2014-10-21 20:03:02 -0600

    Changed some html tags

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('_master')
 
 @section('body')
-  <div class='container'>
+  <div class="container">
     <h1>P3 - Flavio Pereira - Developers's Best Friend</h1>
 
     <div class="row">
@@ -14,8 +14,7 @@
                 text, users, and passwords to demostrate your work. This tool will help you to create those features. For the text, we can use lorem-ipsum
                 text generator, where you can specify how many paragraphs you want. </p>
                 <p> The random users will help you to create users where you can specify the number of users, birthday, address, and some profile
-                description. Moreover, you can use the xkdc password to generate a random password, which allows you to choose from one to four words, one number, 
-                one symbol, and all words with the first letter in uppercase. With that, you are going to create a strong password, making it harder to guess even by brute force.</p>
+                description.</p>
             </div>
             <!-- /.col-md-4 -->
         </div>
@@ -35,14 +34,7 @@
                 description.</p>
                 <a class="btn btn-default" href='user'> Create Users</a>
             </div>
-            <!-- /.col-md-4 -->
-            <div class="col-md-4">
-                <h2>xkcd Password</h2>
-                <p>Generate a random password, which allows you to choose from one to four words, one number, 
-                one symbol, and all words with the first letter in uppercase. With that, you are going to create a strong password, making it harder to guess even by brute force.</p>
-                <a class="btn btn-default" href="xkcd-password">Create Password</a>
-            </div>
-            <!-- /.col-md-4 -->
         </div>
+    </div>
         <!-- /.row -->
 @stop
